80cad3eb24a95e8ac98ff39171ac15841931a536
Max Timkovich <mtimkovich@gmail.com>
2013-02-10 19:10:27 -0600

    Added confirm prompt before backing up files

--- install.pl
+++ install.pl
@@ -3,6 +3,15 @@ use strict;
 use warnings;
 use feature "say";
 
+sub yesno {
+    my ($prompt) = @_;
+
+    print "$prompt [Y/n] ";
+    chomp($_ = <>);
+
+    /y/i;
+}
+
 sub is_folder_empty {
     opendir DH, shift or die $!;
 
@@ -16,17 +25,22 @@ opendir DH, $ENV{PWD} or die $!;
 while (my $file = readdir DH) {
     next if $file =~ /^\.*$/;
     next if $file =~ /README/;
-    next if $file =~ /^\.git/;
+    next if $file eq ".gitignore";
+    next if $file eq ".git";
     next if $0 =~ /$file/;
 
     my $old = "$ENV{PWD}/$file";
     my $new = "$ENV{HOME}/$file";
 
     if (-e $new and not -l $new) {
-        mkdir $backup_dir;
+        if (yesno "$file exists, overwrite?") {
+            mkdir $backup_dir;
 
-        say "Backing up old $file to $backup_dir";
-        rename $new, "$backup_dir/$file";
+            say "Backing up old $file to $backup_dir";
+            rename $new, "$backup_dir/$file";
+        } else {
+            next;
+        }
     }
 
     say "Creating symlink to $file in home directory";
